c5885d9e0fa208cc33cb9fee1b94ad2c40b7e561
Brandy Carney <brandyscarney@gmail.com>
2016-01-06 14:35:16 -0500

    fix(app): update tabs starter to include an ion-nav element to work with overlay components
    references driftyco/ionic2#877

--- app/app.js
+++ app/app.js
@@ -1,7 +1,5 @@
 import {App, Platform} from 'ionic/ionic';
-import {Page1} from './pages/page1/page1';
-import {Page2} from './pages/page2/page2';
-import {Page3} from './pages/page3/page3';
+import {TabsPage} from './pages/tabs/tabs';
 
 
 @App({
@@ -9,12 +7,7 @@ import {Page3} from './pages/page3/page3';
 })
 export class MyApp {
   constructor(platform: Platform) {
-
-    // this tells the tabs component which Pages
-    // should be each tab's root Page
-    this.tab1Root = Page1;
-    this.tab2Root = Page2;
-    this.tab3Root = Page3;
+    this.root = TabsPage;
 
     platform.ready().then(() => {
       // Do any necessary cordova or native calls here now that the platform is ready
