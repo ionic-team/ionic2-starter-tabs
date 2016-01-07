f1d447d6c495b55ea310ea93557fa0268c31cf18
mhartington <mikehartington@gmail.com>
2016-01-07 11:28:18 -0500

    config

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
-import {App, Platform} from 'ionic/ionic';
+import {App, Platform, Config} from 'ionic/ionic';
 import {TabsPage} from './pages/tabs/tabs';
 
 
 @App({
-  templateUrl: 'build/app.html'
+  templateUrl: 'build/app.html',
+  // Check out the config API docs for more info
+  // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}
 })
 export class MyApp {
   constructor(platform: Platform) {
