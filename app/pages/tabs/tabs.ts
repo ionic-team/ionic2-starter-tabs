950365fcec7cd6c775b297efcd39ba0ebf636e9a
mhartington <mikehartington@gmail.com>
2016-01-08 10:04:38 -0500

    fix(app): update tabs starter to include an ion-nav element to work with overlay components

---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import {Page1} from '../page1/page1';
+import {Page2} from '../page2/page2';
+import {Page3} from '../page3/page3';
+
+
+@Page({
+  templateUrl: 'build/pages/tabs/tabs.html'
+  // Check out the config API docs for more info
+  // http://ionicframework.com/docs/v2/api/config/Config/
+})
+export class TabsPage {
+  constructor() {
+    // this tells the tabs component which Pages
+    // should be each tab's root Page
+    this.tab1Root = Page1;
+    this.tab2Root = Page2;
+    this.tab3Root = Page3;
+  }
+}
