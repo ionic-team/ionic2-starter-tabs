6489bb0bed73a15573d492734e199783b807901a
Tim Lancina <tim@ionic.io>
2016-01-20 11:18:08 -0600

    more tweaks

--- app/app.ts
+++ app/app.ts
@@ -6,15 +6,28 @@ import {Type} from 'angular2/core';
 
 
 @App({
-  template: '<ion-nav id="nav" [root]="root" #content></ion-nav>',
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  root: Type = TabsPage;
+  rootPage: Type = TabsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Do any necessary cordova or native calls here now that the platform is ready
+      // The platform is now ready. Note: if this callback fails to fire, follow
+      // the Troubleshooting guide for a number of possible solutions:
+      //
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      //
+      // First, let's hide the keyboard accessory bar (only works natively) since
+      // that's a better default:
+      //
+      // Keyboard.setAccessoryBarVisible(false);
+      //
+      // For example, we might change the StatusBar color. This one below is
+      // good for dark backgrounds and light text:
+      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
     });
   }
 }
