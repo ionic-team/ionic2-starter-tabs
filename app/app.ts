4ac4c919e51ba62c34a1ff27ac4b770940057cc0
Tim Lancina <tim@ionic.io>
2016-01-20 09:55:35 -0600

    chore(): fix type errors

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,20 @@
-import {App, Platform, Config} from 'ionic-framework/ionic';
+import {App, Platform} from 'ionic-framework/ionic';
 import {TabsPage} from './pages/tabs/tabs';
 
+// https://angular.io/docs/ts/latest/api/core/Type-interface.html
+import {Type} from 'angular2/core';
+
+
 @App({
-  template: '<ion-nav id="nav" [root]="root" #content></ion-nav>'
+  template: '<ion-nav id="nav" [root]="root" #content></ion-nav>',
   // Check out the config API docs for more info
   // http://ionicframework.com/docs/v2/api/config/Config/
   config: {}
 })
 export class MyApp {
-  constructor(platform: Platform) {
-    this.root = TabsPage;
+  root: Type = TabsPage;
 
+  constructor(platform: Platform) {
     platform.ready().then(() => {
       // Do any necessary cordova or native calls here now that the platform is ready
     });
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -3,16 +3,21 @@ import {Page1} from '../page1/page1';
 import {Page2} from '../page2/page2';
 import {Page3} from '../page3/page3';
 
+// https://angular.io/docs/ts/latest/api/core/Type-interface.html
+import {Type} from 'angular2/core';
+
 
 @Page({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
 export class TabsPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  tab1Root: Type = Page1;
+  tab2Root: Type = Page2;
+  tab3Root: Type = Page3;
+
   constructor() {
-    // this tells the tabs component which Pages
-    // should be each tab's root Page
-    this.tab1Root = Page1;
-    this.tab2Root = Page2;
-    this.tab3Root = Page3;
+
   }
 }
