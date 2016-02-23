4a7dd01557a729e84cca5c0f8b49b51e9a3c44f7
Tim Lancina <tim@ionic.io>
2016-02-23 16:03:48 -0600

    chore(): remove unused constructors

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -4,8 +4,4 @@ import {Page} from 'ionic-framework/ionic';
 @Page({
   templateUrl: 'build/pages/page1/page1.html'
 })
-export class Page1 {
-  constructor() {
-
-  }
-}
+export class Page1 {}
--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -4,8 +4,4 @@ import {Page} from 'ionic-framework/ionic';
 @Page({
   templateUrl: 'build/pages/page2/page2.html'
 })
-export class Page2 {
-  constructor() {
-
-  }
-}
+export class Page2 {}
