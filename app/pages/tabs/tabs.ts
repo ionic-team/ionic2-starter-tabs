521ac9dba6a3b5ef737956c3b0fa7e79d20ab566
Tim Lancina <tim@ionic.io>
2016-02-23 19:07:06 -0600

    chore(): remove Type in favor of any.
    Type doesn't really offer any more information than any and requires an
    Angular import.

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -3,9 +3,6 @@ import {Page1} from '../page1/page1';
 import {Page2} from '../page2/page2';
 import {Page3} from '../page3/page3';
 
-// https://angular.io/docs/ts/latest/api/core/Type-interface.html
-import {Type} from 'angular2/core';
-
 
 @Page({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -13,7 +10,7 @@ import {Type} from 'angular2/core';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: Type = Page1;
-  tab2Root: Type = Page2;
-  tab3Root: Type = Page3;
+  tab1Root: any = Page1;
+  tab2Root: any = Page2;
+  tab3Root: any = Page3;
 }
