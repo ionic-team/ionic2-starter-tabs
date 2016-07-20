e0490b968deecbc05b8e0ed4ce71623299518895
Ibrahim Hadeed <ihadeed@users.noreply.github.com>
2016-07-20 15:25:14 -0400

    Update contact.ts

--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -5,6 +5,6 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {
-  constructor(private nav: NavController) {
+  constructor(private navCtrl: NavController) {
   }
 }
