ee13b24e4f1b4a453467b0f5ecbc7e27b9e48bdb
Brandy Carney <brandyscarney@gmail.com>
2016-09-19 20:35:38 -0400

    fix(app): update app to use new bundle file and fix dev file

--- src/app/main.dev.ts
+++ src/app/main.dev.ts
@@ -1,6 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './app.module.ngfactory';
 
-enableProdMode();
-platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory);
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
