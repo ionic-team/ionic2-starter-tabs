24e86e9651f69dd87819f8a9d5b2fb1423792970
Brandy Carney <brandyscarney@gmail.com>
2016-09-20 22:29:12 -0400

    refactor(app): add app files (testing merge)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {
 
   }
 }
