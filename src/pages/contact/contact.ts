7b2da63059882bdabf9fd3913e3863cfffc062e9
Justin Willis <jgw9617@gmail.com>
2016-09-21 22:52:52 -0500

    chore(): change private to public

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  constructor(private navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {
 
   }
 }
