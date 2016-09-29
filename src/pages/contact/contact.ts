c68fdd6864d2f0590b63a88365be826c207eb591
Adam Bradley <adambradley25@gmail.com>
2016-09-29 02:22:24 +0100

    add page selectors

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
+  selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+
   constructor(public navCtrl: NavController) {
 
   }
+
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
+  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
   constructor(public navCtrl: NavController) {
 
   }
+
 }
