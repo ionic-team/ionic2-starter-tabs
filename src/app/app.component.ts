6ab52a2ea7ae8b1c9dfc0237c5b49ba9f4ca82fc
jgw96 <jgw9617@gmail.com>
2017-03-27 15:22:22 -0500

    chore(code-splitting): all pages now code-split

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
-
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = 'TabsPage';
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
