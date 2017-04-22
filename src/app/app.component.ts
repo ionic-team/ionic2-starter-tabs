a6a36b2a1efeac707ef8befb8bea66da710bc04e
Daniel Sogl <MTechDE@users.noreply.github.com>
2017-04-22 18:28:09 +0200

    Update app.component.ts
    In your side menu template, you are using a separate method to initialize the app. So in the this template it should be the same.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,18 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  constructor(private platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
