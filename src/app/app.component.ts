fdaad1117a15c3fa01f51562cfaff9b59955ff53
Daniel Sogl <daniel@sogls.de>
2017-05-30 21:14:52 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,9 @@ export class MyApp {
   rootPage: any = TabsPage;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
   }
 
-  initializeApp() {
+  ionViewDidLoad() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
